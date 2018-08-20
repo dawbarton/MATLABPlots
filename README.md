3d6b46ea2a6f8e6188e8de095bcfcc1679ea3845
David AW Barton <david.barton@bristol.ac.uk>
2018-08-20 16:22:49 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # MATLABPlots
+
+This package implements a thin wrapper over the
+(MATLAB.jl)[https://github.com/JuliaInterop/MATLAB.jl] to provide a MATLAB
+plotting interface from the comfort of Julia. For example,
+
+```julia
+x = range(0, stop=2π, length=201)
+y = sin.(x)
+plot(x, y, ".-")
+```
+
+At the moment only a few of the MATLAB plotting functions are wrapped. More may
+be added in time as needs arise. The functions wrapped so far are
+
+* `figure`
+* `clf`
+* `plot`
+    * Note: only style specification is possible; additional arguments in the
+      `plot(x, y, style, "Keyword", "Value")` MATLAB style are not recognised yet.
+	* `plot` is overloaded to plot in 3D if a `z` variable is passed.
+* `plot!`
+    * This plots with the hold set to true.
--- src/MATLABPlots.jl
+++ src/MATLABPlots.jl
@@ -1,5 +1,63 @@
 module MATLABPlots
 
-# package code goes here
+using MATLAB
+
+export MATLAB, figure, plot, plot!, clf
+
+function figure()
+    mxcall(:figure, 0)
+end
+
+function figure(i::Int)
+    mxcall(:figure, 0, i)
+end
+
+function clf()
+    mxcall(:clf, 0)
+end
+
+function clf(i::Int)
+    mxcall(:clf, 0, i)
+end
+
+function plot(i::Int, x::AbstractArray, y::AbstractArray, style="-")
+    mxcall(:hold, 0, i, "off")
+    mxcall(:plot, 0, i, x, y, style)
+end
+
+function plot(i::Int, x::AbstractArray, y::AbstractArray, z::AbstractArray, style="-")
+    mxcall(:hold, 0, i, "off")
+    mxcall(:plot3, 0, i, x, y, z, style)
+end
+
+function plot(x::AbstractArray, y::AbstractArray, style="-")
+    mxcall(:hold, 0, "off")
+    mxcall(:plot, 0, x, y, style)
+end
+
+function plot(x::AbstractArray, y::AbstractArray, z::AbstractArray, style="-")
+    mxcall(:hold, 0, "off")
+    mxcall(:plot3, 0, x, y, z, style)
+end
+
+function plot!(i::Int, x::AbstractArray, y::AbstractArray, style="-")
+    mxcall(:hold, 0, i, "on")
+    mxcall(:plot, 0, i, x, y, style)
+end
+
+function plot!(i::Int, x::AbstractArray, y::AbstractArray, z::AbstractArray, style="-")
+    mxcall(:hold, 0, i, "on")
+    mxcall(:plot3, 0, i, x, y, z, style)
+end
+
+function plot!(x::AbstractArray, y::AbstractArray, style="-")
+    mxcall(:hold, 0, "on")
+    mxcall(:plot, 0, x, y, style)
+end
+
+function plot!(x::AbstractArray, y::AbstractArray, z::AbstractArray, style="-")
+    mxcall(:hold, 0, "on")
+    mxcall(:plot3, 0, x, y, z, style)
+end
 
 end # module
